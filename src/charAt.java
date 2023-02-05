f2b2afbaa43ec0bfe083e68a0a214f6cc4cf016b
RAMSINGHPARUL <210303126179@paruluniversity.ac.in>
2023-02-05 22:52:24 +0530

    Create charAt.java

---
+++ src/charAt.java
@@ -0,0 +1,9 @@
+public class charAt
+{
+    public static void main(String args[])
+    {
+        String name="javatpoint";
+        char ch=name.charAt(4);
+        System.out.println(ch);
+    }
+}
